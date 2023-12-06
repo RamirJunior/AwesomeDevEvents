7e2d3e6b1c1c403e93eb0731f1cfe5e4207ba94b
Ramir Junior <junior.ribeiro0@live.com>
2023-12-06 12:56:12 -0300

    Add project files.

---
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,61 @@
+﻿using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEvents.API.Controllers {
+    
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase {
+        private readonly DevEventsDbContext _context;
+        public DevEventsController(DevEventsDbContext context) {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() {
+            var devEvents = _context.DevEvents.Where(d => !d.IsDeleted).ToList();
+
+            return Ok(devEvents);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id) {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+                return NotFound();
+
+            return Ok(devEvent);
+        }
+
+        [HttpPost]
+        public IActionResult Post(DevEvent devEvent) {
+            _context.DevEvents.Add(devEvent);
+
+            return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, DevEvent input) {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+                return NotFound();
+
+            devEvent.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id) {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+                return NotFound();
+
+            devEvent.Delete();
+            return NoContent();
+        }
+    }
+}
